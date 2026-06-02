4b2da322045d33babb2bee79c2f030ee0c564b67
anjali21i <anjaliguptajnp21@gmail.com>
2026-06-03 00:43:32 +0530

    db setup

--- src/services/OfflineDatabaseService.ts
+++ src/services/OfflineDatabaseService.ts
@@ -1,5 +1,7 @@
 import type {AuthLog} from '../types/LogTypes';
+import type {User} from '../types/UserTypes';
 import {authLogRepository} from './database/repositories/authLogRepository';
+import {userRepository} from './database/repositories/userRepository';
 import {getLocalDatabase} from './database/localDatabase';
 
 export class OfflineDatabaseService {
@@ -9,6 +11,22 @@ export class OfflineDatabaseService {
 
   async saveAuthLog(authLog: AuthLog): Promise<void> {
     await authLogRepository.save(authLog);
+  }
+
+  async saveUser(user: User): Promise<void> {
+    await userRepository.save(user);
+  }
+
+  async getUsers(): Promise<User[]> {
+    return userRepository.getAll();
+  }
+
+  async getUserByEmployeeId(employeeId: string): Promise<User | null> {
+    return userRepository.findByEmployeeId(employeeId);
+  }
+
+  async getPendingSyncUsers(): Promise<User[]> {
+    return userRepository.getPendingSync();
   }
 
   async getOfflineLogs(): Promise<AuthLog[]> {
@@ -24,6 +42,13 @@ export class OfflineDatabaseService {
     syncStatus: AuthLog['syncStatus'],
   ): Promise<void> {
     await authLogRepository.updateSyncStatus(id, syncStatus);
+  }
+
+  async markUserSyncStatus(
+    employeeId: string,
+    syncStatus: User['syncStatus'],
+  ): Promise<void> {
+    await userRepository.updateSyncStatus(employeeId, syncStatus);
   }
 }
 
