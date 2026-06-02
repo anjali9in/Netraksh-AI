9d3978753ce5a581015a78b59e6da49242090282
anjali21i <anjaliguptajnp21@gmail.com>
2026-06-03 03:09:28 +0530

    prettier removed

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,5 +1,61 @@
 module.exports = {
   root: true,
-  extends: '@react-native-community',
+  env: {
+    es6: true,
+    jest: true,
+    node: true,
+  },
+  extends: ['eslint:recommended', 'plugin:react/recommended', 'plugin:react-hooks/recommended'],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 2020,
+    sourceType: 'module',
+  },
+  plugins: ['@typescript-eslint', 'jest', 'react', 'react-hooks', 'react-native'],
+  settings: {
+    react: {
+      version: 'detect',
+    },
+  },
   ignorePatterns: ['backend/dist/**', 'backend/.aws-sam/**'],
+  globals: {
+    __DEV__: 'readonly',
+  },
+  overrides: [
+    {
+      files: ['*.js'],
+      parser: '@babel/eslint-parser',
+      parserOptions: {
+        requireConfigFile: false,
+      },
+    },
+    {
+      files: ['*.ts', '*.tsx'],
+      parser: '@typescript-eslint/parser',
+      rules: {
+        '@typescript-eslint/no-shadow': 'warn',
+        '@typescript-eslint/no-unused-vars': [
+          'error',
+          {
+            argsIgnorePattern: '^_',
+            destructuredArrayIgnorePattern: '^_',
+            ignoreRestSiblings: true,
+          },
+        ],
+        'no-shadow': 'off',
+        'no-undef': 'off',
+        'no-unused-vars': 'off',
+      },
+    },
+  ],
+  rules: {
+    curly: 'warn',
+    'no-bitwise': 'warn',
+    'no-case-declarations': 'off',
+    'no-console': 'off',
+    'react/prop-types': 'off',
+    'react-native/no-inline-styles': 'warn',
+  },
 };
